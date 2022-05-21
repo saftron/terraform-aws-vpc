8590792bb7100a7145f0cd4fc6063043e054a828
jagadheesan <saftron@gmail.com>
2022-05-21 10:38:00 -0400

    Added Route Table Association resource

--- main.tf
+++ main.tf
@@ -95,3 +95,15 @@ resource "aws_route_table" "private" {
     Name = "private${count.index}"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
