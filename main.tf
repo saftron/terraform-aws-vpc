c229f436f9b214c7db51a650b643b85366007287
jagadheesan <saftron@gmail.com>
2022-05-21 11:55:15 -0400

    Updated line 100

--- main.tf
+++ main.tf
@@ -97,7 +97,9 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+  count = 2 
+  
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
