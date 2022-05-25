67b06938f3b1308d7d1c33ee24226dc917ea492e
jagadheesan <saftron@gmail.com>
2022-05-24 21:55:44 -0400

    Updated count meta argument & variable file updates

--- main.tf
+++ main.tf
@@ -101,14 +101,14 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = 2
+  count = length(local.public_cidr)
 
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count = 2
+  count = length(local.private_cidr)
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
