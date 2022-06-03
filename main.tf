1295c97aebca5d82c938553dfabbee4c6db51b67
jagadheesan <saftron@gmail.com>
2022-06-02 21:44:03 -0400

    first EC2 instance & user data

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "var.region"
+  region = "us-east-1"
 }
 
 # Create a VPC
--- variables.tf
+++ variables.tf
@@ -1,32 +1,36 @@
 variable "env_code" {
   type    = string
- }
-
- variable "region" {
-  type = string
+  default = "development"
 }
 
 variable "public" {
   type    = string
+  default = "FE"
 }
 
 variable "private" {
   type    = string
+  default = "DB"
 }
 
 variable "igw" {
   type    = string
+  default = "igw"
 }
 
 variable "ngw" {
   type    = string
+  default = "ngw"
 }
 
 variable "rt" {
   type    = string
+  default = "RTPublic"
 }
 
 variable "rtp" {
   type    = string
+  default = "RTPrivate"
+
 }
 
