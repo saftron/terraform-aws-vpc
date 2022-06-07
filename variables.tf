73f201b75d4141cf2588a8a3e7145c085cf3cf3c
Vladimir Mukhin <awsvlad@gmail.com>
2022-06-07 18:35:00 -0400

    Remove unsed variable

--- variables.tf
+++ variables.tf
@@ -7,34 +7,3 @@ variable "env_code" {
   type        = string
   default = "training"
 }
-
-variable "public" {
-  type        = string
-  default = "FrontEnd"
-}
-
-variable "private" {
-  type        = string
-  default = "BackEnd"
-}
-
-variable "igw" {
-  type        = string
-  default = "main"
-}
-
-variable "ngw" {
-  type        = string
-  default = "main"
-}
-
-variable "rt" {
-  type        = string
-  default = "public"
-}
-
-variable "rtp" {
-  type        = string
-  default = "private"
-}
-
