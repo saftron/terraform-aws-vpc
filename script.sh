a151ec44ef605faf3250b7894be4541e23b6d63e
jagadheesan <saftron@gmail.com>
2022-06-16 20:36:58 -0400

    new line on scripts added

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 yum update -y
 yum install -y httpd
 systemctl start httpd && systemctl enable httpd
-echo >> “Hello mydevopsmentor from $(hostname -f). Created from Terraform” > /var/www/html/index.html 
+echo >> “Hello mydevopsmentor from $(hostname -f). Created from Terraform” >> /var/www/html/index.html 
