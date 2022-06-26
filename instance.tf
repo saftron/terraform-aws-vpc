526f31de697ebc47e81f69a0dd3a0181803fb3dc
jagadheesan <saftron@gmail.com>
2022-06-26 19:17:28 -0400

    Removed locals & dynamic blocks

--- instance.tf
+++ instance.tf
@@ -14,32 +14,33 @@ data "aws_ami" "amazonlinux" {
 
 }
 
-locals {
-  ingress_rules = [{
-    port        = 443
-    description = "Ingress rules for port 443"
-    },
-    {
-      port        = 80
-      description = "Ingree rules for port 80"
-  }]
-}
-
 resource "aws_security_group" "tf_sg" {
   name        = "${var.vpc_name}-default"
   description = "The ID of the VPC that the instance security group belongs to."
   vpc_id      = aws_vpc.main.id
 
-  dynamic "ingress" {
-    for_each = local.ingress_rules
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [var.my_public_ip]
+  }
 
-    content {
-      description = ingress.value.description
-      from_port   = ingress.value.port
-      to_port     = ingress.value.port
-      protocol    = "tcp"
-      cidr_blocks = [var.my_public_ip]
-    }
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [var.my_public_ip]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.my_public_ip]
   }
 
   egress {
@@ -47,7 +48,6 @@ resource "aws_security_group" "tf_sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
