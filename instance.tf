0610261b5faab3d1802fb7e8fa0a9159f9e8792e
Vladimir Mukhin <awsvlad@gmail.com>
2022-07-02 22:12:28 -0400

    Open port 80 in a security group

--- instance.tf
+++ instance.tf
@@ -57,6 +57,14 @@ resource "aws_security_group" "tf_sg" {
     cidr_blocks = [var.my_public_ip]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
