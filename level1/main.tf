457625a6f32cb1386470b2193cd4740341b1a005
jagadheesan <saftron@gmail.com>
2022-07-14 18:32:55 -0400

    new changes on layered approach

--- level1/main.tf
+++ level1/main.tf
@@ -19,6 +19,10 @@ resource "aws_vpc" "main" {
   }
 }
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
 locals {
   public_cidr  = ["10.0.0.0/24", "10.0.1.0/24"]
   private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
