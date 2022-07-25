8618aeefa0d93b12e889a1f5748faa7c5e50118d
jagadheesan <saftron@gmail.com>
2022-07-24 20:09:10 -0400

    Added SG under my LB

--- level2/alb.tf
+++ level2/alb.tf
@@ -3,9 +3,10 @@ resource "aws_lb" "jag-test-lb" {
   internal           = false
   load_balancer_type = "application"
   ip_address_type    = "ipv4"
-  subnets            = ["data.terraform_remote_state.level1.outputs.public_subnet_id[0]", "data.terraform_remote_state.level1.outputs.public_subnet_id[1]"]
+  security_groups    = [aws_security_group.tf_sg.id]
+  subnets            = [data.terraform_remote_state.level1.outputs.public_subnet_id[0], data.terraform_remote_state.level1.outputs.public_subnet_id[1]]
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = {
     Name = "my-test-alb"
@@ -18,10 +19,30 @@ resource "aws_lb_target_group" "jag-alb-tg" {
   protocol    = "HTTP"
   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
   target_type = "instance"
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "20"
+    unhealthy_threshold = "2"
+    timeout             = "10"
+    path                = "/"
+    port                = "80"
+    protocol            = "HTTP"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "test" {
   target_group_arn = aws_lb_target_group.jag-alb-tg.arn
   target_id        = aws_instance.web.id
   port             = 80
+}
+
+resource "aws_lb_listener" "lb_listener_http" {
+  load_balancer_arn = aws_lb.jag-test-lb.id
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    target_group_arn = aws_lb_target_group.jag-alb-tg.id
+    type             = "forward"
+  }
 }
